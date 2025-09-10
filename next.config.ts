c117a6538035c3780802a57bc9703a50a3684542
wbinghao <wbinghao@linewell.com>
2025-09-10 18:18:14 +0800

    Add GitHub Pages deployment support

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["react", "react-dom"],
   },
+  // 为 GitHub Pages 配置
+  images: {
+    unoptimized: true,
+  },
+  // 如果您的仓库不是在根域名下，请修改为您的仓库名称
+  // 例如：如果仓库是 https://github.com/username/my-blog，则设置为 '/my-blog/'
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
